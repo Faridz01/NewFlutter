4b12a0eb760bb338aa1738c500d2ff3a1c0fb41d
Faridz01 <fariz_241@smkassalambandung.sch.id>
2023-02-25 03:01:03 +0700

    menyiapkan fungsi untuk memanggil API

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -1,20 +1,34 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:tokoonline/app/data/entertainment_response.dart';
+import 'package:tokoonline/app/data/headline_response.dart';
+import 'package:tokoonline/app/data/sports_response.dart';
+import 'package:tokoonline/app/data/technology_response.dart';
+import 'package:tokoonline/app/utils/api.dart';
 
 class DashboardController extends GetxController {
-  //TODO: Implement DashboardController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  final getConnect = GetConnect();
+  final auth = GetStorage();
+  
+  Future<HeadlineResponse> getHeadline() async {
+    final response = await getConnect.get(BaseUrl.headline);
+    return HeadlineResponse.fromJson(jsonDecode(response.body));
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  Future<TechnologyResponse> getTechnology() async {
+    final response = await getConnect.get(BaseUrl.technology);
+    return TechnologyResponse.fromJson(jsonDecode(response.body));
   }
 
-  @override
-  void onClose() {}
-  void increment() => count.value++;
+  Future<SportsResponse> getSport() async {
+    final response = await getConnect.get(BaseUrl.sports);
+    return SportsResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<EntertainmentResponse> getEntertainment() async {
+    final response = await getConnect.get(BaseUrl.entertainment);
+    return EntertainmentResponse.fromJson(jsonDecode(response.body));
+  }
 }
